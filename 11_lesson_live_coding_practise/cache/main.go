5d5aa4e2c06f60b16503fc90322d211346ab2ba6
Daniel Kotelnikov <Danielkotelnikov20@gmail.com>
2024-09-13 00:08:46 +0300

    feat: improve 11 lesson

--- 11_lesson_live_coding_practise/cache/main.go
+++ 11_lesson_live_coding_practise/cache/main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"context"
 	"errors"
+	"fmt"
 	"sync"
 	"time"
 )
 
-type RedisDatabase interface {
-	Get(context.Context, string) (string, error)
-	MGet(context.Context, []string) ([]*string, error)
-	Keys(context.Context) ([]string, error)
+type IRedisDb interface {
+	Add(key, value string)
+	Get(ctx context.Context, key string) (string, error)
+	MGet(ctx context.Context, keys []string) ([]*string, error)
+	Keys(ctx context.Context) ([]string, error)
 }
 
 // Questions?????
@@ -18,32 +20,18 @@ type RedisDatabase interface {
 // - sync no more, than 1 minute
 // - goroutine safe
 
-type RedisDatabaseWithCache struct {
-	mutex sync.RWMutex
-	data  map[string]string
-
-	operationsMutex  sync.Mutex
-	activeOperations map[string]chan struct{}
-
-	database RedisDatabase
+type RedisCache struct {
+	data sync.Map
+	db   IRedisDb
 }
 
-func NewRedisDatabaseWithCache(ctx context.Context, database RedisDatabase) (*RedisDatabaseWithCache, error) {
-	if database == nil {
-		return nil, errors.New("invalid arguments")
-	}
-
-	db := &RedisDatabaseWithCache{
-		activeOperations: make(map[string]chan struct{}),
-		data:             make(map[string]string),
-		database:         database,
-	}
-
-	go db.synchronize(ctx)
-	return db, nil
+func NewRedisCache(ctx context.Context, db IRedisDb) (*RedisCache, error) {
+	cache := &RedisCache{db: db}
+	go cache.synchronize(ctx)
+	return cache, nil
 }
 
-func (c *RedisDatabaseWithCache) synchronize(ctx context.Context) {
+func (r *RedisCache) synchronize(ctx context.Context) {
 	ticker := time.NewTicker(time.Minute)
 	defer ticker.Stop()
 
@@ -51,134 +39,133 @@ func (c *RedisDatabaseWithCache) synchronize(ctx context.Context) {
 		select {
 		case <-ctx.Done():
 			return
-		default:
-		}
-
-		select {
-		case <-ctx.Done():
-			return
 		case <-ticker.C:
-			c.syncImpl(ctx)
+			r.syncImplement(ctx)
 		}
 	}
 }
 
-func (c *RedisDatabaseWithCache) syncImpl(ctx context.Context) {
-	var err error
+func (r *RedisCache) syncImplement(ctx context.Context) {
+	var data []*string
 	var keys []string
-	err = withRetry(ctx, 3, time.Millisecond*100, func() error {
-		keys, err = c.database.Keys(ctx)
-		return err
-	})
-
-	if err != nil || len(keys) == 0 {
-		return
-	}
-
-	var values []*string
-	err = withRetry(ctx, 3, time.Millisecond*100, func() error {
-		values, err = c.database.MGet(ctx, keys)
-		return err
-	})
-
-	if err != nil {
-		return
-	}
-
-	data := make(map[string]string, len(keys))
-	for idx, key := range keys {
-		value := values[idx]
-		if value != nil {
-			data[key] = *value
-		}
-	}
-
-	c.mutex.Lock()
-	c.data = data
-	c.mutex.Unlock()
-}
-
-// Get need to proxy only one method from interface
-func (c *RedisDatabaseWithCache) Get(ctx context.Context, key string) (string, error) {
-	var found bool
-	var value string
-	withLock(c.mutex.RLocker(), func() {
-		value, found = c.data[key]
-	})
-
-	if found {
-		return value, nil
-	}
-
-	var barrier chan struct{}
-	var isActiveOperation bool
-	withLock(&c.operationsMutex, func() {
-		if barrier, isActiveOperation = c.activeOperations[key]; !isActiveOperation {
-			barrier = make(chan struct{})
-			c.activeOperations[key] = barrier
-		}
-	})
-
-	if isActiveOperation {
-		<-barrier
-		withLock(c.mutex.RLocker(), func() {
-			value, found = c.data[key]
-		})
-
-		return value, nil
-	}
-
-	defer func() {
-		close(barrier)
-		withLock(&c.operationsMutex, func() {
-			delete(c.activeOperations, key)
-		})
-	}()
-
 	var err error
-	err = withRetry(ctx, 3, time.Millisecond*100, func() error {
-		value, err = c.database.Get(ctx, key)
+
+	if err := withRetry(ctx, 3, time.Millisecond*100, func() error {
+		keys, err = r.db.Keys(ctx)
 		return err
-	})
-
-	if err != nil {
-		return "", err
-	}
-
-	withLock(&c.mutex, func() {
-		c.data[key] = value
-	})
-
-	return value, nil
-}
-
-func withLock(locker sync.Locker, action func()) {
-	if action == nil {
+	}); err != nil || len(keys) == 0 {
 		return
 	}
 
-	locker.Lock()
-	action()
-	locker.Unlock()
+	err = withRetry(ctx, 3, time.Millisecond*100, func() error {
+		data, err = r.db.MGet(ctx, keys)
+		return err
+	})
+
+	for index, key := range keys {
+		if index < len(data) && data[index] != nil {
+			r.data.Store(key, data[index])
+		}
+	}
 }
 
-func withRetry(ctx context.Context, retriesNumber int, delay time.Duration, action func() error) error {
-	if action == nil || retriesNumber <= 0 || delay < 0 {
+func (r *RedisCache) Add(key, value string) {
+	r.data.Store(key, value)
+}
+
+func (r *RedisCache) Get(ctx context.Context, key string) (string, error) {
+	if data, exists := r.data.Load(key); exists {
+		return data.(string), nil
+	} else {
+		return "", errors.New("key not found")
+	}
+}
+
+func (r *RedisCache) MGet(ctx context.Context, keys []string) ([]*string, error) {
+	var data []*string
+
+	for _, key := range keys {
+		if value, exists := r.data.Load(key); exists {
+			value := value.(string)
+			data = append(data, &value)
+		}
+	}
+
+	return data, nil
+}
+
+func (r *RedisCache) Keys(ctx context.Context) ([]string, error) {
+	var keys []string
+
+	r.data.Range(func(key, value interface{}) bool {
+		keys = append(keys, key.(string))
+		return true
+	})
+
+	return keys, nil
+}
+
+func withRetry(ctx context.Context, retries int, delay time.Duration, action func() error) error {
+	var err error
+
+	if action == nil || retries <= 0 || delay < 0 {
 		return errors.New("invalid arguments")
 	}
 
-	var err error
-	for retry := 1; retry <= retriesNumber; retry++ {
+	for i := 0; i < retries; i++ {
 		if ctx.Err() != nil {
-			return err
+			return ctx.Err()
 		}
 
 		if err = action(); err == nil {
 			return nil
 		}
 
-		time.Sleep(time.Duration(retry) * delay)
+		time.Sleep(time.Duration(i+1) * delay)
 	}
 
 	return err
+}
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	redis := &RedisCache{}
+	cache, err := NewRedisCache(ctx, redis.db)
+
+	if err != nil {
+		fmt.Println("Error creating RedisCache:", err)
+		return
+	}
+
+	cache.Add("key1", "value1")
+	cache.Add("key2", "value2")
+	cache.Add("key3", "value3")
+	cache.Add("key4", "value4")
+
+	if data, err := cache.Get(ctx, "key1"); err == nil {
+		fmt.Println("Get data from cache:", data)
+	} else {
+		fmt.Println("Error:", err)
+	}
+
+	keys := []string{"key1", "key2", "key3", "key4"}
+	if data, err := cache.MGet(ctx, keys); err == nil {
+		for key, value := range data {
+			if value != nil {
+				fmt.Printf("MGet %s: %s\n", keys[key], *value)
+			} else {
+				fmt.Printf("MGet %s: not found\n", keys[key])
+			}
+		}
+	} else {
+		fmt.Println("Error:", err)
+	}
+
+	if keys, err := cache.Keys(ctx); err == nil {
+		fmt.Println("Keys in cache:", keys)
+	} else {
+		fmt.Println("Error:", err)
+	}
 }
